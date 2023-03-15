82bf8307f88fc20197f2fd74d6cadcccd2e2330c
Moixie <royelpos@gmail.com>
2023-03-15 18:13:33 +0800

    MigrateTables

--- database/migrations/2023_03_15_075615_create_products_table.php
+++ database/migrations/2023_03_15_075615_create_products_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +15,15 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug');
+            $table->string('image')->nullable();
+            $table->string('image_mime')->nullable();
+            $table->string('image)size')->nullable();
+            $table->string('description')->nullable();
+            $table->decimal('price',10,2);
+            $table->foreignIdFor(User::class,'created_by')->nullable();
+            $table->foreignIdFor(User::class,'updated_by')->nullable();
+            $table->softDeletes();
+            $table->foreignIdFor(User::class,'deleted_by')->nullable();
             $table->timestamps();
         });
     }
