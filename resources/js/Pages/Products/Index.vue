810e92be3805af7824e27b8e32dec1cbd767b4ca
Moixie <royelpos@gmail.com>
2023-03-20 13:07:44 +0800

    index

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -1,28 +1,18 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import ProductAddDialog from '@/Components/product/ProductAddDialog.vue';
 import { VDataTable } from 'vuetify/labs/VDataTable'
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { useForm, Head } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 
 defineProps(['products','categories']);
 </script>
 
 <script>
 
-let form = useForm({
-    file: null,
-    product_name: '',
-    price:'',
-    quantity: '',
-    category_id: '',
-
-   
-});
 export default {
     data: () => ({
-      selectItems: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-
       dialog: false,
       dialogDelete: false,
       search: '',
@@ -33,7 +23,7 @@ export default {
           sortable: false,
           key: 'product_name',
         },
-        { title: 'Image', key: 'file' },
+        { title: 'Image', key: 'image' },
         { title: 'Price', key: 'price' },
         { title: 'Quantity', key: 'quantity' },
         { title: 'Category', key: 'category.title' },
@@ -61,9 +51,7 @@ export default {
     }),
 
     computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Product' : 'Edit Product'
-      },
+    
     },
 
     watch: {
@@ -81,41 +69,7 @@ export default {
 
     methods: {
       initialize () {
-        // this.desserts = [
-        //   {
-        //     name: 'Frozen Yogurt',
-        //     file: '',
-        //     price: 6.0,
-        //     quantity: 24,
-        //     category_id: 'School Supply',
-        //     status: 'Low',
-        //   },
-        //   {
-        //     name: 'Ice cream sandwich',
-        //     file: '',
-        //     price: 9.0,
-        //     quantity: 37,
-        //     category_id: 'School Supply',
-        //     status: 'Low',
-        //   },
-        //   {
-        //     name: 'Eclair',
-        //     file: '',
-        //     price: 16.0,
-        //     quantity: 23,
-        //     category_id: 'School Supply',
-        //     status: 'Low',
-        //   },
-        //   {
-        //     name: 'Cupcake',
-        //     file: '',
-        //     price: 3.7,
-        //     quantity: 67,
-        //     category_id: 'School Supply',
-        //     status: 'Low',
-        //   },
-      
-        // ]
+     
       },
 
       editItem (item) {
@@ -151,14 +105,14 @@ export default {
         })
       },
 
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          form.post(route('products.store'),{ onSuccess: () => form.reset()})
-        }
-        this.close()
-      },
+      // save () {
+      //   if (this.editedIndex > -1) {
+      //     Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      //   } else {
+       
+      //   }
+      //   this.close()
+      // },
     },
   }
 </script>
@@ -175,13 +129,13 @@ export default {
     :sort-by="[{ key: 'product_name', order: 'asc' }]"
     class="elevation-1"
   >
-  <template v-slot:[`item.file`]`="{ item }">
+  <template v-slot:[`item.image`]`="{ item }">
   
   <v-avatar
       size="100"
       rounded="0"
     >
-      <v-img cover :src="'http://127.0.0.1:8000/'+item.columns.file"></v-img>
+      <v-img cover :src="'http://127.0.0.1:8000/'+item.columns.image"></v-img>
     </v-avatar> 
        <!-- <img :src="'http://127.0.0.1:8000/'+item.file" alt=""> -->
         </template>
@@ -204,114 +158,14 @@ export default {
         single-line
         hide-details
       ></v-text-field>
-        <v-dialog
-          v-model="dialog"
-          max-width="500px"
-          persistent
-        >
-          <template v-slot:activator="{ props }">
-            
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="props"
-            >
-              New Product
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
 
-            <v-card-text>
-              <form @submit.prevent="submitForm">
-              <v-container>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
-                  >
-                  <v-file-input
-                  v-model="form.file"
-                    label="Product Image"
-                    density="compact"
-                  ></v-file-input>
-                  </v-col>
 
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
-                  >
-                    <v-text-field
-                      v-model="form.product_name"
-                      label="Dessert name"
-                     
-                    ></v-text-field>
-                  </v-col>
-                
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="form.price"
-                      label="Price"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="form.quantity"
-                      label="Quantity"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="12"
-                  >
 
-                  <v-select
-                    v-model="form.category_id"
-                    :items="categories"
-                    item-text="title"
-                    item-value="id"
-                    label="Category"
-                    density="comfortable"
-                  ></v-select>
-                  </v-col>
-                </v-row>
-              </v-container>
-              </form>
-            </v-card-text>
+        <ProductAddDialog>
+          </ProductAddDialog>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+
+
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
@@ -324,18 +178,7 @@ export default {
           </v-card>
         </v-dialog>
 
-        <v-dialog v-model="addCategory" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5">New Category</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-
+      
 
       </v-toolbar>
     </template>
