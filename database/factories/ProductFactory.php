df29912efdfdd62f4fe6d0f4a594a9053a8c3bc2
Moixie <royelpos@gmail.com>
2023-04-12 16:46:23 +0800

    product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,19 +17,15 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            // 'name' => fake()->name(),
-            // 'email' => fake()->unique()->safeEmail(),
-            // 'email_verified_at' => now(),
-            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            // 'remember_token' => Str::random(10),
-
-            // 'user_id' => 1,
-            // 'product_name' => fake()->word(),
-            // 'image' => fake()->imageUrl(),
-            // 'status' => fake()->randomElement(['School Supply', 'Others', 'Printing']),
-            // 'price' => fake()->randomFloat(2, 1, 100),
-            // 'quantity' => fake()->numberBetween(1, 100),
-            // 'category_id' => fake()->numberBetween(1, 5),
+          
+            'user_id' => 1,
+            'product_name' => fake()->word(),
+            'image' => fake()->imageUrl(),
+            'stock_level' => fake()->numberBetween(1, 100),
+            'unit' => fake()->numberBetween(1, 100),
+          
+            'unit_type_id' => fake()->numberBetween(1, 5),
+            'category_id' => fake()->numberBetween(1, 5),
            
         ];
     }
