4569c514a4af08c76df4691b94d53250817f30af
Moixie <royelpos@gmail.com>
2023-04-12 16:47:31 +0800

    index

--- resources/js/Pages/Purchases/Index.vue
+++ resources/js/Pages/Purchases/Index.vue
@@ -1,14 +1,29 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { VDataTableServer } from 'vuetify/labs/VDataTable'
 import { VDataTable } from 'vuetify/labs/VDataTable'
-import InputError from '@/Components/InputError.vue';
 import { router } from '@inertiajs/vue3'
 import {useForm, Head } from '@inertiajs/vue3';
+import PurchaseRestockDialog from '@/Components/purchases/PurchaseRestockDialog.vue';
 
-defineProps(['purchases']);
+defineProps(['purchases','products']);
 </script>
 
 <script>
+
+
+const restockForm = useForm ({
+    product_id: '',
+    original_price: '',
+    sale_price: '',
+    quantity: '',
+    purchasedDate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    expirationDate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+
+})
+
+//Date Format Function
+//Convert Numerical date format to readable date 
 function formatDate(dateString) {
   const months = [
     "January", "February", "March", "April", "May", "June",
@@ -20,6 +35,8 @@ function formatDate(dateString) {
   const year = date.getFullYear();
   return `${month} ${day}, ${year}`;
 }
+
+
 export default {
   data: () => ({
     dialog: false,
@@ -36,28 +53,12 @@ export default {
       { title: 'Expiration Date', key: 'expired_at'},
       { title: 'Actions', key: 'actions', sortable: false },
     ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
-    defaultItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
+  
+  
   }),
 
   computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Purchase' : 'Edit Purchase'
-    },
+    
   },
 
   watch: {
@@ -79,15 +80,12 @@ export default {
     },
 
     editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+   
+   
     },
 
     deleteItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
+  
     },
 
     deleteItemConfirm () {
@@ -95,34 +93,29 @@ export default {
       this.closeDelete()
     },
 
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
+   
     closeDelete () {
       this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
+  
     },
 
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      this.close()
+   saveRestock() {
+         this.restockForm.post(route('purchases.store'), {
+        onSuccess: () => {
+          this.restockForm.reset()
+        },
+        onError: () => {
+          // this.dialog= true
+        } 
+
+      });
+      this.dialog= true
     },
   },
 }
 </script>
 <template>
+   <!-- :items-length="purchases.total" (For Data-table-server)--> 
     <Head title="Purchases" />
  
     <AuthenticatedLayout>
@@ -131,6 +124,7 @@ export default {
     :headers="headers"
     :items="purchases.data"
     :search="search"
+   
     :sort-by="[{ key: 'purchased_at', order: 'desc' }]"
     class="elevation-1"
   >
@@ -185,101 +179,17 @@ export default {
         single-line
         hide-details
       ></v-text-field>
-        <v-dialog
-          v-model="dialog"
-          max-width="500px"
-        >
-          <template v-slot:activator="{ props }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="props"
-            >
-              New Purchase
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
+       
+      <PurchaseRestockDialog
+      :products="products"
+      :purchases="purchases"
+      :restockForm="restockForm"
+      :dialog="dialog"
+      @saveRestock="saveRestock"
+      >
+    </PurchaseRestockDialog>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Dessert name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
